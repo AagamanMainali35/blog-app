93600a3b4050f56ccd9cc353f24ba258d6bf7d3b
AagamanMainali <aagamanmainali35@gmail.com>
2024-08-26 22:35:38 +0545

    chnages to models made

--- core/models.py
+++ core/models.py
@@ -4,8 +4,7 @@ class tags(models.Model):
     Tagname=models.CharField(verbose_name='tagname',max_length=50,blank=False,null=True)
     def __str__(self):
         return(self.Tagname)
-    
-
+ 
 class blog(models.Model):
     Username=models.CharField(verbose_name='username',max_length=50,blank=False,null=True)
     BlogTitle=models.CharField(verbose_name='Blogtitle',max_length=50,blank=False,null=True)
