790f22da684056639dfa426f88a4bbd40d0d497c
AagamanMainali <aagamanmainali35@gmail.com>
2024-08-26 22:35:54 +0545

    changes to model class

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ class tags(models.Model):
     def __str__(self):
         return(self.Tagname)
  
+ 
 class blog(models.Model):
     Username=models.CharField(verbose_name='username',max_length=50,blank=False,null=True)
     BlogTitle=models.CharField(verbose_name='Blogtitle',max_length=50,blank=False,null=True)
